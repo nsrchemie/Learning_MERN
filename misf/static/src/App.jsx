abc75440384136b8db22d343edfdd01fea1caff9
nsrchemie <nsrchemie@gmail.com>
2017-04-11 18:50:21 -0400

    adding more react components

--- misf/static/src/App.jsx
+++ misf/static/src/App.jsx
@@ -1,9 +1,34 @@
   const contentNode = document.getElementById('contents');
   
+  class IssueFilter extends React.Component {
+     render() {
+       return (
+        <div>Filter</div>
+)}}
+
+  class IssueTable extends React.Component {
+     render() {
+       return (
+        <div>Table</div>
+)}}
+
+  class IssueAdd extends React.Component {
+     render() {
+       return (
+        <div>Add</div>
+)}}
   class IssueList extends React.Component {
      render() {    
        return (
-	<div>Placeholder</div>);
+	<div>
+	<h1>Issue Tracker</h1>
+	<IssueFilter />
+	<hr />
+	<IssueTable />
+	<hr />
+	<IssueAdd />
+	</div>
+	);
     }}
 
   ReactDOM.render(<IssueList />, contentNode);
