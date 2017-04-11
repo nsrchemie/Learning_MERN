1abe090ae36b73ddce74a047285b6949f5346841
nsrchemie <nsrchemie@gmail.com>
2017-04-11 17:29:09 -0400

    working with react classes

--- misf/static/src/App.jsx
+++ misf/static/src/App.jsx
@@ -1,6 +1,10 @@
   const contentNode = document.getElementById('contents');
-  const continents = ['Africa','America','Asia','Australia','Europe'];
-  const message = continents.map(c => `Hello ${c}!`).join(' '); 
-  const component = <p>{message}</p>;
-  ReactDOM.render(component, contentNode);
+  
+  class IssueList extends React.Component {
+     render() {    
+       return (
+	<div>Placeholder</div>);
+    }}
+
+  ReactDOM.render(<IssueList />, contentNode);
 
