4fb01ee387f9c3b854c0b3e6e59048705a6e2460
nsrchemie <nsrchemie@gmail.com>
2017-04-11 19:21:41 -0400

    adding properties

--- misf/static/src/App.jsx
+++ misf/static/src/App.jsx
@@ -6,10 +6,35 @@
         <div>Filter</div>
 )}}
 
+ class IssueRow extends React.Component {
+   render() {
+     const borderedStyle = {border: "1px solid silver", padding: 4};
+     return (
+      <tr>
+        <td style={borderedStyle}>{this.props.issue_id}</td>
+	<td style={borderedStyle}>{this.props.issue_title}</td>
+     </tr>
+)}}
+
+
   class IssueTable extends React.Component {
      render() {
-       return (
-        <div>Table</div>
+      const borderedStyle = {border: "1px solid silver", padding: 6};
+	 return (
+          <table style={{borderCollapse: "collapse"}}>
+	   <thead>
+	    <tr>
+	     <th style={borderedStyle}>Id</th>
+	     <th style={borderedStyle}>Title</th>
+	    </tr>
+	  </thead>
+	 <tbody>
+	   <IssueRow issue_id={1}
+	    issue_title="Error in console when clicking Add" />
+	   <IssueRow issue_id={2}
+	    issue_title="Missing bottom border" />
+         </tbody>
+	</table>
 )}}
 
   class IssueAdd extends React.Component {
