39d7019cba5aad1382c7aa5e920e2ac6850bb1b3
nsrchemie <nsrchemie@gmail.com>
2017-04-16 14:42:22 -0400

    had to switch to react-router-dom for router v4, 8000 works but some error in render

--- misf/static/src/App.jsx
+++ misf/static/src/App.jsx
@@ -2,19 +2,22 @@ import 'babel-polyfill';
 import IssueList from './IssueList.jsx';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, Redirect, hashHistory } from 'react-router';
+// import { Router, Route, Redirect, hashHistory } from 'react-router';
+import { HashRouter, Route, Redirect } from 'react-router-dom';
 import IssueEdit from './IssueEdit.jsx';
 
 const contentNode = document.getElementById('contents');
 const NoMatch = () =><p>Page Not Found</p>;
 
 const RoutedApp = () => (
-  <Router history={hashHistory} >
+  <HashRouter>
+   <div>
     <Redirect from="/" to="/issues" />
     <Route path="/issues" component={IssueList} />
     <Route path="/issues/:id" component={IssueEdit} />
     <Route path="*" component={NoMatch} />
-   </Router>
+   </div>
+  </HashRouter>
 );
 
 ReactDOM.render(<RoutedApp />, contentNode);
